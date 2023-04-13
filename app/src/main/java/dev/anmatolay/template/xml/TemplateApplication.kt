be3a4e11194b8ab9ec4ea2dcb58bb2fcd18ca2e0
Anna Matolay <anmatolay@gmail.com>
2023-04-13 03:55:03 +0200

    feat: enhance firebase CrashlyticsLogTree

--- app/src/main/java/dev/anmatolay/template/xml/TemplateApplication.kt
+++ app/src/main/java/dev/anmatolay/template/xml/TemplateApplication.kt
@@ -1,12 +1,18 @@
 package dev.anmatolay.template.xml
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dev.anmatolay.template.xml.di.KoinInitializer
-import dev.anmatolay.template.xml.util.logging.CrashReportingTree
+import dev.anmatolay.template.xml.util.logging.CrashlyticsLogTree
 import dev.anmatolay.template.xml.util.logging.DiamondDebugTree
+import org.koin.android.ext.android.inject
 import timber.log.Timber
 
 class TemplateApplication : Application() {
+
+    private val firebaseCrashlytics by inject<FirebaseCrashlytics>()
 
     override fun onCreate() {
         super.onCreate()
@@ -17,7 +23,7 @@ class TemplateApplication : Application() {
             if (BuildConfig.DEBUG)
                 DiamondDebugTree()
             else
-                CrashReportingTree()
+                CrashlyticsLogTree(firebaseCrashlytics)
         )
     }
 }
---
+++ app/src/main/java/dev/anmatolay/template/xml/util/logging/CrashlyticsLogTree.kt
@@ -0,0 +1,29 @@
+package dev.anmatolay.template.xml.util.logging
+
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.crashlytics.ktx.setCustomKeys
+import timber.log.Timber
+
+class CrashlyticsLogTree(private val firebaseCrashlytics: FirebaseCrashlytics) : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if (priority < Log.ERROR) return
+
+//        firebaseCrashlytics.setUserId()
+        firebaseCrashlytics.setCustomKeys {
+            tag?.let { key(CRASHLYTICS_KEY_TAG, it) }
+            key(CRASHLYTICS_KEY_MESSAGE, message)
+        }
+
+        if (t != null) {
+            firebaseCrashlytics.recordException(t)
+        } else {
+            firebaseCrashlytics.log("$priority/$tag: $message")
+        }
+    }
+
+    companion object {
+        private const val CRASHLYTICS_KEY_TAG = "tag"
+        private const val CRASHLYTICS_KEY_MESSAGE = "message"
+    }
+}
